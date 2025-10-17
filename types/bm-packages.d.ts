93b2038d455082cdcf982f06f973ac6bf07fa5d1
Giancarlo Villena <g_villena@icloud.com>
2025-10-17 09:07:09 -0500

    fix: align ts configs and exports for package aliases

---
+++ types/bm-packages.d.ts
@@ -0,0 +1,27 @@
+declare module "@bm/domain" {
+  export * from "../packages/domain/src/index.js";
+}
+
+declare module "@bm/policies" {
+  export * from "../packages/policies/src/index.js";
+}
+
+declare module "@bm/runtime" {
+  export * from "../packages/runtime/src/index.js";
+}
+
+declare module "@bm/aria" {
+  export * from "../packages/aria/src/index.js";
+}
+
+declare module "@bm/sdk" {
+  export * from "../packages/sdk/src/index.js";
+}
+
+declare module "@bm/validation" {
+  export * from "../packages/validation/src/index.js";
+}
+
+declare module "@bm/ui" {
+  export * from "../packages/ui/src/index.js";
+}
